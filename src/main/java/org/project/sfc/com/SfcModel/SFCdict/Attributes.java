0a7096d3958113707612ec286aca49769d3c0dd1
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-12 23:02:20 +0530

    Update Attributes.java

--- src/main/java/org/project/sfc/com/SfcModel/SFCdict/Attributes.java
+++ src/main/java/org/project/sfc/com/SfcModel/SFCdict/Attributes.java
@@ -3,5 +3,7 @@ package org.project.sfc.com.SfcModel.SFCdict;
 
 import javax.annotation.Generated;
 
+// org.jsonschema2pojo is to make Java objects from JSON
+// probably used for parsing NSD JSON
 @Generated("org.jsonschema2pojo")
 public class Attributes {}
